e71711b785db7b4a0930c8828aa4f0f49b55b1e3
Igor Shagaev <igor.shagaev15@gmail.com>
2018-06-03 09:04:48 +0300

    Fix

---
+++ SalaryLibrary/Employee.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Salary
+{
+    public abstract class Employee
+    {
+        /// <summary>
+        /// Имя работника
+        /// </summary>
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                Regex regex = new Regex("[А-ЯЁ][а-яё]");
+                Match match = regex.Match(value);
+                if (match.Success)
+                {
+                    _name = Name;
+                }
+            }
+        }
+        /// <summary>
+        /// Фамилия работника
+        /// </summary>
+        private string _surname;
+        public string Surname
+        {
+            get
+            {
+                return _surname;
+            }
+            set
+            {
+                Regex regex = new Regex("[А-ЯЁ][а-яё]");
+                Match match = regex.Match(value);
+                if (match.Success)
+                {
+                    _surname = value;
+                }
+            }
+        }
+        /// <summary>
+        /// Время работы
+        /// </summary>
+        private int _workTime;
+        public int WorkTime
+        {
+            get
+            {
+                return _workTime;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Некорректное время работы");
+                }
+                _workTime = value;
+            }
+        }
+        public abstract int GetSalary();
+        public Employee (string name, string surname, int workTime)
+        {
+            Name = name;
+            Surname = surname;
+            WorkTime = workTime;
+        }
+    }
+}
+    
---
+++ SalaryLibrary/EmployeeHourly.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salary
+{
+    /// <summary>
+    /// Зарплата по часам
+    /// </summary>
+    public class EmployeeHourly : Employee
+    {
+        /// <summary>
+        /// Оплата за час
+        /// </summary>
+        private int _costPerHour;
+        public int CostPerHour
+        {
+            get
+            {
+                return _costPerHour;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Некорректная оплата за час");
+                }
+                _costPerHour = value;
+            }
+        }
+        /// <summary>
+        /// Подсчет зарплаты
+        /// </summary>
+        public override int GetSalary()
+        {
+            const int hoursPerDay = 24;
+            int salary = WorkTime / hoursPerDay * CostPerHour;
+            return salary;
+        }
+        public EmployeeHourly(string name, string surname, int workTime, int costPerHour)
+            : base(name, surname, workTime)
+        {
+            CostPerHour = costPerHour;
+        }
+    }
+}
