8857407f9f573caf964b44bdd5abd65972f41f06
Igor Shagaev <igor.shagaev15@gmail.com>
2018-06-03 17:11:03 +0300

    Add second project

---
+++ Salary/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salary
+{
+    public abstract class Employee
+    {
+        /// <summary>
+        /// Время работы
+        /// </summary>
+        private int _workTime;
+        public int WorkTime
+        {
+            get
+            {
+                return _workTime;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Некорректное время работы");
+                }
+                _workTime = value;
+            }
+        }
+        public abstract int GetSalary();
+        public Employee(int workTime)
+        {
+            WorkTime = workTime;
+        }
+    }
+}
---
+++ Salary/EmployeeRate.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salary
+{
+    /// <summary>
+    /// Зарплата по ставке
+    /// </summary>
+    public class EmployeeRate : Employee
+    {
+        private int _salary;
+        public int Salary
+        {
+            get
+            {
+                return _salary;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Некорректный оклад");
+                }
+                _salary = value;
+            }
+        }
+        /// <summary>
+        /// Ставка (норма работы в месяц)
+        /// </summary>
+        private int _rate;
+        public int Rate
+        {
+            get
+            {
+                return _rate;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Некорректная ставка");
+                }
+                _rate = value;
+            }
+        }
+        /// <summary>
+        /// Подсчет зарплаты по ставке
+        /// </summary>
+        public override int GetSalary()
+        {
+            int salary = _salary / _rate * WorkTime;
+            return salary;
+        }
+        public EmployeeRate(int workTime, int salary, int rate)
+            : base(workTime)
+        {
+            _salary = salary;
+            _rate = rate;
+        }
+    }
+}
